381a771b080c8eaf3d6f240065020115c3bfd0fc
Logan Holt <WolfGuardLegion11@gmail.com>
2020-10-23 10:40:49 -0700

    Math Practice

--- MathForGames/MathForGames/MathForGamesStart.cs
+++ MathForGames/MathForGames/MathForGamesStart.cs
@@ -8,9 +8,9 @@ namespace MathForGames
         {
             Console.WriteLine("Hello World!");
         }
-        static float mathFunction(float x)
+        static float mathFunction(float d)
         {
-            float result = x * x + 2 * x - 7;
+            float result = d * d + 2 * d - 7;
             return result;
         }
         static float mathFunction(float a, float b, float c)
@@ -19,5 +19,18 @@ namespace MathForGames
             float result = (float)(-b +- Math.Sqrt(b * b + 4 * a * c) / 2 * a);
             return result;
         }
+
+        static float MathFunction(float s, float e, float t)
+        {
+            float result = s + t * (e - s);
+            return result;
+        }
+
+        static float mathFunction(float x1, float x2, float y1, float y2)
+        {
+            float result = (float)(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2- y1)));
+            return result;
+        }
+
     }
 }
