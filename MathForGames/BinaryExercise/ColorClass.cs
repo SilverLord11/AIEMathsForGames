a80754930b891109f3b3350f7b53c17cd4612247
Logan Holt <WolfGuardLegion11@gmail.com>
2020-10-23 10:15:49 -0700

    Color Class Practice

--- MathForGames/BinaryExercise/ColorClass.cs
+++ MathForGames/BinaryExercise/ColorClass.cs
@@ -6,6 +6,21 @@ namespace BinaryExercise
 {
     class ColorClass
     {
+        public UInt32 colour;
+        UInt32 value = colour & 0xff000000;
+        
+        byte red = (byte)(value >> 24);
 
+        public void SetRed(byte red)
+        {
+            colour = colour & 0x00ffffff;
+            colour |= (UInt32)red << 24;
+        }
+
+        public byte GetRed()
+        {
+            return (byte)((colour & 0xff000000) >> 24);
+            UInt32 value = colour & 0xff000000;
+        }
     }
 }
